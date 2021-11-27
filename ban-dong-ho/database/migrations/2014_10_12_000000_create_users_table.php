825c1f6eb5849e1aa199ab7647e82b1249cf8805
azdoan1604 <thaodoan.2x06@gmail.com>
2021-11-27 14:53:05 +0700

    ịuoi

--- ban-dong-ho/database/migrations/2014_10_12_000000_create_users_table.php
+++ ban-dong-ho/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('HOTEN');
+            $table->integer("GIOITINH");
+            $table->date("NGAYSINH");
+            $table->string("SDT");
+            $table->string('email',100)->unique();
+            $table->string('name')->nullable();
+            $table->string('password')->nullable();
+            $table->integer('TRANGTHAI')->nullable();
+            $table->string('DIACHI')->nullable();
+            $table->string('GHICHU')->nullable();
+            $table->integer('MATINH')->nullable();
+            $table->integer('MAQUAN_HUYEN')->nullable();
+            $table->integer('MAPHUONG_XA')->nullable();
+            $table->integer('VAITRO')->nullable();
+            $table->string('FACEBOOK_ID')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });

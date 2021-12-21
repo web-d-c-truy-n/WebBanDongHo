c84c23c1a214e8762ddf9beaca7dc717b3f06927
azdoan1604 <thaodoan.2x06@gmail.com>
2021-12-21 16:02:18 +0700

    fhdfh

--- ban-dong-ho/resources/views/pages/information_account.blade.php
+++ ban-dong-ho/resources/views/pages/information_account.blade.php
@@ -1,78 +1,16 @@
 @extends('layout')
 @section('rederbody')
 @section('css')
-<link rel="icon" href="{{asset('templateAdmin/assets/img/brand/favicon.png')}}" type="image/png">
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
-        <!-- Icons -->
-        <link rel="stylesheet" href="{{asset('templateAdmin/assets/vendor/nucleo/css/nucleo.css')}}" type="text/css">
-        <link rel="stylesheet" href="{{asset('templateAdmin/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css')}}" type="text/css">
-        <!-- Page plugins -->
-        <!-- Argon CSS -->
-        <link rel="stylesheet" href="{{asset('templateAdmin/assets/css/argon.css?v=1.2.0')}}" type="text/css">
+  <link rel="stylesheet" href="{{asset('css/argon.css')}}" type="text/css"/>
 @endsection
 <div class="container-fluid mt--6">
   <div class="row">
-    <div class="col-xl-4 order-xl-2">
-      <div class="card card-profile">
-        <img src="../assets/img/theme/img-1-1000x600.jpg" alt="Image placeholder" class="card-img-top">
-        <div class="row justify-content-center">
-          <div class="col-lg-3 order-lg-2">
-            <div class="card-profile-image">
-              <a href="#">
-                <img src="../assets/img/theme/team-4.jpg" class="rounded-circle">
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
-          <div class="d-flex justify-content-between">
-            <a href="#" class="btn btn-sm btn-info  mr-4 ">Connect</a>
-            <a href="#" class="btn btn-sm btn-default float-right">Message</a>
-          </div>
-        </div>
-        <div class="card-body pt-0">
-          <div class="row">
-            <div class="col">
-              <div class="card-profile-stats d-flex justify-content-center">
-                <div>
-                  <span class="heading">22</span>
-                  <span class="description">Friends</span>
-                </div>
-                <div>
-                  <span class="heading">10</span>
-                  <span class="description">Photos</span>
-                </div>
-                <div>
-                  <span class="heading">89</span>
-                  <span class="description">Comments</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="text-center">
-            <h5 class="h3">
-              Jessica Jones<span class="font-weight-light">, 27</span>
-            </h5>
-            <div class="h5 font-weight-300">
-              <i class="ni location_pin mr-2"></i>Bucharest, Romania
-            </div>
-            <div class="h5 mt-4">
-              <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
-            </div>
-            <div>
-              <i class="ni education_hat mr-2"></i>University of Computer Science
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-xl-8 order-xl-1">
+    <div class="col-md-12">
       <div class="card">
         <div class="card-header">
           <div class="row align-items-center">
             <div class="col-8">
-              <h3 class="mb-0">Edit profile </h3>
+              <h3 class="mb-0">Chỉnh sửa </h3>
             </div>
             <div class="col-4 text-right">
               <a href="#!" class="btn btn-sm btn-primary">Settings</a>

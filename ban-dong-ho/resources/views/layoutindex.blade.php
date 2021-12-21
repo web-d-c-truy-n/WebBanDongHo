a320ef4329276013f49927af7946341b7756682d
azdoan1604 <thaodoan.2x06@gmail.com>
2021-12-21 15:37:29 +0700

    dgdg

--- ban-dong-ho/resources/views/layoutindex.blade.php
+++ ban-dong-ho/resources/views/layoutindex.blade.php
@@ -176,7 +176,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                         <li class="dropdown1"><a href="{{URL::to("/dang-nhap")}}" class="openModal">{{Auth::user()->name??"TÀI KHOẢN"}}</a>
                             @if (Auth::user())
                                 <ul pclass="dropdown2">
-                                    <li><a>Thông tin</a></li>
+                                    <li><a href="{{URL::to('/thong-tin-tai-khoan')}}">Thông tin</a></li>
                                     <li><a>Tra cứu đơn hàng</a></li>
                                     <li><a>Đăng xuất</a></li>
                                 </ul>

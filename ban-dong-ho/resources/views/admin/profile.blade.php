0d84b842c6c8c3a7112072e7a25cda8d5f7a7e4a
azdoan1604 <thaodoan.2x06@gmail.com>
2021-12-24 11:14:55 +0700

    Update profile.blade.php

--- ban-dong-ho/resources/views/admin/profile.blade.php
+++ ban-dong-ho/resources/views/admin/profile.blade.php
@@ -4,14 +4,10 @@
     <div class="row">
       <div class="col-xl-4 order-xl-2">
         <div class="card card-profile">
-          <img src="../assets/img/theme/img-1-1000x600.jpg" alt="Image placeholder" class="card-img-top">
+          
           <div class="row justify-content-center">
             <div class="col-lg-3 order-lg-2">
-              <div class="card-profile-image">
-                <a href="#">
-                  <img src="../assets/img/theme/team-4.jpg" class="rounded-circle">
-                </a>
-              </div>
+              
             </div>
           </div>
           <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
@@ -41,16 +37,13 @@
             </div>
             <div class="text-center">
               <h5 class="h3">
-                Jessica Jones<span class="font-weight-light">, 27</span>
+                Họ tên<span class="font-weight-light"></span>
               </h5>
               <div class="h5 font-weight-300">
-                <i class="ni location_pin mr-2"></i>Bucharest, Romania
+                <i class="ni location_pin mr-2"></i>Điện thoại
               </div>
-              <div class="h5 mt-4">
-                <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
-              </div>
-              <div>
-                <i class="ni education_hat mr-2"></i>University of Computer Science
+              <div class="h5 font-weight-300">
+                <i class="ni location_pin mr-2"></i>Mail
               </div>
             </div>
           </div>
@@ -61,86 +54,143 @@
           <div class="card-header">
             <div class="row align-items-center">
               <div class="col-8">
-                <h3 class="mb-0">Edit profile </h3>
+                <h3 class="mb-0">Thông tin cá nhân</h3>
               </div>
               <div class="col-4 text-right">
-                <a href="#!" class="btn btn-sm btn-primary">Settings</a>
+                <a href="#!" class="btn btn-sm btn-primary">Chỉnh sửa</a>
               </div>
             </div>
           </div>
           <div class="card-body">
             <form>
-              <h6 class="heading-small text-muted mb-4">User information</h6>
+              <h6 class="heading-small text-muted mb-4">Thông tin cá nhân</h6>
               <div class="pl-lg-4">
                 <div class="row">
                   <div class="col-lg-6">
                     <div class="form-group">
                       <label class="form-control-label" for="input-username">Username</label>
-                      <input type="text" id="input-username" class="form-control" placeholder="Username" value="lucky.jesse">
+                      <input type="text" id="input-username" class="form-control" placeholder="Username" value="">
                     </div>
                   </div>
                   <div class="col-lg-6">
                     <div class="form-group">
-                      <label class="form-control-label" for="input-email">Email address</label>
-                      <input type="email" id="input-email" class="form-control" placeholder="jesse@example.com">
+                      <label class="form-control-label" for="input-email">Mail</label>
+                      <input type="email" id="input-email" class="form-control" placeholder="Mail">
                     </div>
                   </div>
                 </div>
                 <div class="row">
                   <div class="col-lg-6">
                     <div class="form-group">
-                      <label class="form-control-label" for="input-first-name">First name</label>
-                      <input type="text" id="input-first-name" class="form-control" placeholder="First name" value="Lucky">
+                      <label class="form-control-label" for="input-first-name">Họ và tên</label>
+                      <input type="text" id="input-first-name" class="form-control" placeholder="Name" value="">
                     </div>
                   </div>
                   <div class="col-lg-6">
                     <div class="form-group">
-                      <label class="form-control-label" for="input-last-name">Last name</label>
-                      <input type="text" id="input-last-name" class="form-control" placeholder="Last name" value="Jesse">
+                      <label class="form-control-label" for="input-first-name">Điện thoại</label>
+                      <input type="tel" id="input-first-name" class="form-control" placeholder="Phone" value="">
                     </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-lg-6">
+                    <div class="form-group">
+                      <label class="form-control-label" for="input-first-name">Ngày sinh</label>
+                      <input type="date" class="form-control">
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="form-group">
+                      <label class="form-control-label" for="input-first-name">Giới tính</label>
+                      <select class="form-control"  style="margin-top:0px;">
+                        <option>Nam</option>
+                        <option>Nữ</option>
+                        <option>Khác</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                
+                <div class="row">
+                  <div class="col-lg-6">
+                    <div class="form-group">
+                      
+                      <label class="form-control-label" for="input-first-name">Mật khẩu cũ</label>
+                      <input type="password" id="input-first-name" class="form-control" placeholder="Password" value="">
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="form-group">
+                      <label class="form-control-label" for="input-first-name">Mật khẩu mới</label>
+                      <input type="password" id="input-first-name" class="form-control" placeholder="Password" value="">
+                    </div>
+                  </div>
+                </div>
+                <a href="#!" class="btn btn-sm btn-primary" style="margin-left:auto;margin-right:auto;display:block;margin-bottom:0%">Đổi mật khẩu</a>
+                <div class="row">
+                  <div class="col-md-12">
+                    <label class="form-control-label" for="input-first-name">Facebook</label>
+                    <input type="text" id="input-first-name" class="form-control" placeholder="Facebook ID" value="">
                   </div>
                 </div>
               </div>
               <hr class="my-4">
               <!-- Address -->
-              <h6 class="heading-small text-muted mb-4">Contact information</h6>
+              <h6 class="heading-small text-muted mb-4">Thông tin liên hệ</h6>
               <div class="pl-lg-4">
                 <div class="row">
                   <div class="col-md-12">
                     <div class="form-group">
-                      <label class="form-control-label" for="input-address">Address</label>
-                      <input id="input-address" class="form-control" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" type="text">
+                      <label class="form-control-label" for="input-address">Địa chỉ</label>
+                      <input id="input-address" class="form-control" placeholder="Andress" value="" type="text">
                     </div>
                   </div>
                 </div>
                 <div class="row">
                   <div class="col-lg-4">
                     <div class="form-group">
-                      <label class="form-control-label" for="input-city">City</label>
-                      <input type="text" id="input-city" class="form-control" placeholder="City" value="New York">
+                      <label class="form-control-label" for="input-city">Tỉnh/ Thành phố</label><br/>
+                      <select class="field-input form-control" id="tinhThanh" name="MATINH">
+                        <option data-code="null" value="null" selected="">
+                            Chọn tỉnh / thành </option>   
+          
+                              <option value=""></option>
+                                        
+                    </select>
                     </div>
                   </div>
                   <div class="col-lg-4">
                     <div class="form-group">
-                      <label class="form-control-label" for="input-country">Country</label>
-                      <input type="text" id="input-country" class="form-control" placeholder="Country" value="United States">
+                      <label class="form-control-label" for="input-country">Quận/ Huyện</label><br/>
+                      <select class="field-input form-control" id="tinhThanh" name="MAQUAN_HUYEN">
+                        <option data-code="null" value="null" selected="">
+                            Chọn quận/ huyện </option>   
+                              <option value=""></option>
+                                        
+                    </select>
                     </div>
                   </div>
                   <div class="col-lg-4">
                     <div class="form-group">
-                      <label class="form-control-label" for="input-country">Postal code</label>
-                      <input type="number" id="input-postal-code" class="form-control" placeholder="Postal code">
+                      <label class="form-control-label" for="input-country">Xã/ Phường/ Thị trấn</label>
+                      <select class="field-input form-control" id="tinhThanh" name="MAPHUONG_XA">
+                        <option data-code="null" value="null" selected="">
+                            Chọn xã/ phường/ thị trấn </option>   
+                              <option value=""></option>
+                                        
+                    </select>
                     </div>
                   </div>
                 </div>
               </div>
               <hr class="my-4">
               <!-- Description -->
-              <h6 class="heading-small text-muted mb-4">About me</h6>
+              <h6 class="heading-small text-muted mb-4">Giới thiệu</h6>
               <div class="pl-lg-4">
                 <div class="form-group">
                   <label class="form-control-label">About Me</label>
-                  <textarea rows="4" class="form-control" placeholder="A few words about you ...">A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</textarea>
+                  <textarea rows="4" class="form-control" placeholder=""></textarea>
                 </div>
               </div>
             </form>
